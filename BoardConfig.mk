40dee8473f47c6eea7a31ad6eb5f0035ecf83877
xlxfoxxlx <joshfox87@gmail.com>
2017-02-26 13:06:06 -0500

    Aosip-ify

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -71,8 +71,11 @@ TARGET_KERNEL_APPEND_DTB := true
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/oneplus/msm8996
-TARGET_KERNEL_CONFIG := du_oneplus3_defconfig
+TARGET_KERNEL_CONFIG := elementalx_defconfig
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+
+# UberTc Toolchain
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9-kernel/bin
 
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
